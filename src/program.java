6c3260e7782a66d871d9e0f627101cc510255f71
jihanfachrani <jihan.fachrani@unida.ac.id>
2021-11-20 11:09:12 +0800

    belajar program java sederhana

---
+++ src/program.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class program {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		String nama;
+		int tahun;
+		 Scanner masukan= new Scanner(System.in);
+		 
+		 System.out.println("Assalamualaikum ");
+		 System.out.println("Nama saya : " );
+		 nama = masukan.nextLine();
+		 System.out.print("saya lahir tahun: ");
+	        tahun = masukan.nextInt();
+	       
+			
+		
+	}
+
+}
+
+	
+		
